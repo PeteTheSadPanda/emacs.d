bc4b35231a1a3768d2726d68ab6ad677c2543e5a
Piotr Biegaj <piotr@getsatisfaction.com>
2012-02-17 16:49:37 -0800

    First emacs 24 commit

---
+++ init.el
@@ -0,0 +1,31 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js solarized-theme flymake flymake-ruby)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" default)))
+ '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "log" "tmp" "public/assets" "TAGS" "public/javascripts/i18n" "public/widget/demo")))
+ '(grep-find-ignored-files (quote ("target" ".#*" "*.rbc" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "TAGS" "*.tgz")))
+ '(grep-find-template "find . <X> -type f <F> | xargs grep <C> -nH -e <R>"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ pbiegaj.el
@@ -0,0 +1,52 @@
+;;;; line numbers on the left in a gui
+(global-linum-mode 1)
+
+;; don't iconify on C-z when running in X
+;; or exit emacs (!) when running in Emacs.app
+(when window-system (global-unset-key "\C-z"))
+
+(setq kill-whole-line t)
+(setq confirm-kill-emacs 'yes-or-no-p)
+
+;; 4 Macs
+(setq mac-command-modifier 'meta)
+
+(add-to-list 'load-path "~/.emacs.d/elpa/solarized-theme-0.2")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/solarized-theme-0.2")
+(load-theme 'solarized-dark t)
+
+(push "/usr/local/bin" exec-path)
+;; no more backup files emacs, I got it
+(setq make-backup-files nil)
+;; set font type and size
+(set-default-font "Monaco-9")
+;; Take all the windows in the current frame and shift them over one.
+;; ;;
+;; ;; With 2 windows, effectively switches their positions.
+;; ;;
+;; ;; With 1 window, this is a no-op.
+(defun rotate-windows ()
+  (interactive)
+  (let ((buffers (mapcar 'window-buffer (window-list))))
+    (mapcar* 'set-window-buffer
+             (window-list)
+             (append (cdr buffers) (list (car buffers))))))
+
+;; load flymake-ruby mode for files flagged for ruby-mode
+(custom-set-faces
+ '(flymake-errline ((((class color)) (:background "#ffffd7"))))
+ '(flymake-warnline ((((class color)) (:background "#0a2832")))))
+
+(eval-after-load 'ruby-mode
+  '(progn
+     (add-hook 'ruby-mode-hook 'flymake-ruby-load)))
+
+;;;; spiffy-mode provides a few utilities
+;; (add-to-list 'load-path "~/.emacs.d/vendor/spiffy")
+;; (setq spiffy-enable-minor-mode t)
+;; (require 'spiffy)
+
+;;;; spiffy-textmate-mode has some good stuff, but I don't want the
+;;;; full minor mode since it stomps all over a bunch of default keybindings
+;; (require 'spiffy-textmate-mode)
+;; (global-set-key [(f5)] 'spiffy-tm-grep-project)
