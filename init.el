f2ad17b6f06defec05c7013cdc0550d72680010c
Piotr Biegaj <piotr@getsatisfaction.com>
2012-03-15 11:10:14 -0700

    Add tromey.com's repo to the list of elpa sources

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
